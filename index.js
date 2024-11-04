a13928e9cd4bacc0f0e311832b54cfbf3c460f97
Ngomi Patchamen Diane <patchbardi@yahoo.fr>
2024-11-04 10:34:02 +0000

    modifer

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 import express from 'express';
 import getDatabaseConnection from './db.js';
-import 'dotenv/config'; // Assurez-vous que cela est ici pour charger les variables d'environnement
-const app = express();
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import cors from 'cors';
 
+const app = express();
+app.use(express.json()); // Pour analyser les requêtes JSON
+
+app.use(cors({
+    origin: process.env.HOST, // React-URL
+    // origin: '*', // Everything-URL
+    credentials: true         // Erlaubt das Senden von Cookies, falls benötigt
+}));
 
 app.get('/', async (req, res) => {
     return res.status(200).json({"Hallo Liebe Maman": 'Morgen ist dein Geburstags, Alles gute Zum Geburstags Liebe Maman'});
@@ -20,7 +29,33 @@ app.get('/users', async (req, res) => {
     }
 });
 
+// Anmeldung
+app.post('/api/login', async (req, res) => {
+    const { username, password } = req.body;
+
+    console.log(username);
+    console.log(password);
+
+    const conn = await getDatabaseConnection();
+    let user;
+    try {
+        [user] = await conn.query('SELECT * FROM users WHERE username = ?', [username]);
+    } catch (error) {
+        console.log(error);
+    } finally {
+        conn.release();
+    }
+    if (!user) return res.status(400).json({ error: 'Benutzer nicht gefunden' });
+
+    const passwordMatch = await bcrypt.compare(password, user.password_hash);
+    if (!passwordMatch) return res.status(400).json({ error: 'Falsches Passwort' });
+
+    const token = jwt.sign({ id: user.id, username: user.username }, process.env.JWT_SECRET, { expiresIn: '1h' });
+
+    res.json({ token, userId: user.id });
+});
+
 // Server starten
-const PORT = process.env.PORT || 3000; 
-app.listen(PORT, () => {
+app.listen(process.env.PORT, () => {
+    console.log(`Server läuft auf http://localhost:${process.env.PORT}`);
 });
